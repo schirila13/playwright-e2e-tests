2a4b6fc4119c0c877fe013820cb3c9690ce3332c
schirila13 <chirilasorin13@gmail.com>
2026-04-19 10:10:10 +0300

    new test added

--- tests/demo/elem.handling.makeappt.spec.ts
+++ tests/demo/elem.handling.makeappt.spec.ts
@@ -7,12 +7,42 @@ test.describe("Make Appointment functionality", () => {
     await expect(page).toHaveTitle("CURA Healthcare Service");
     await expect(page.locator(".text-vertical-center h1")).toHaveText("CURA Healthcare Service",);
 
+    /**
+    * ELEMENT: Button, Link
+    *
+    * @actions
+    * 1. ✅ Click
+    * 2. ✅ Press
+    * 3. ✅ Double click
+    * 4. ✅ Right click
+    * 5. ✅ Hover if Link
+    * 6. ✅ [Optionall timeout if slow
+    */
+
     // 2. Click on the Make Appointment
-    await page.getByRole("link", { name: "Make Appointment" }).click();
+   // await page.getByRole("link", { name: "Make Appointment" }).click();
+    //await page.getByRole("link", { name: "Make Appointment" }).press("Enter");
+   // await page.getByRole("link", { name: "Make Appointment" }).dblclick();
+    //await page.getByRole("link", { name: "Make Appointment" }).click({button: "right"});
+    //await page.getByRole("link", { name: "Make Appointment" }).hover();
+    await page.getByRole("link", { name: "Make Appointment" }).click({timeout: 10_000});
+    
+    
+
     await expect(page.getByText("Please login to make")).toBeVisible();
 
     // 3.Successful Login
-    await page.getByLabel("Username").fill("John Doe");
+    //await page.getByLabel("Username").fill("John Doe");
+
+    //Clears and Enter
+    //await page.getByLabel("Username").clear();
+    //await page.getByLabel("Username").fill("John Doe");
+
+    // press sequentially
+    await page.getByLabel("Username").pressSequentially("John Doe", { delay: 300 });
+
+
+
     await page.getByLabel("Password").fill("ThisIsNotAPassword");
     await page.getByRole("button", { name: "Login" }).click();
 
