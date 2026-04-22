9fca6914cbad7dd59f0a54d46d126097e018fd43
schirila13 <chirilasorin13@gmail.com>
2026-04-22 22:18:25 +0300

    add new inventory test file

--- tests/functional/inventory.spec.ts
+++ tests/functional/inventory.spec.ts
@@ -8,7 +8,18 @@ import{test, expect} from "@playwright/test";
  */
  
  test.describe("Inventory feature", () => {
-    test.beforeEach("Login with valid creds", async () => {});
+    test.beforeEach("Login with valid creds", async ({page}) => {
+
+        //Launch the Url
+        await page.goto('https://www.saucedemo.com/');
+        //Login with valid creds
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+
+        // Assert that we are on the inventory page
+        await expect(page.locator('.title')).toHaveText("Products");
+    });
 
     test("Should confirm all prices are non-zero", async ({ page }) => {})
  })
