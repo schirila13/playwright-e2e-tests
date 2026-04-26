66a59540611b54bde6faca2c54b7d7334f2dea51
schirila13 <chirilasorin13@gmail.com>
2026-04-26 11:29:51 +0300

    add new inventory test file

---
+++ tests/demo/debug.makeapt.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Make Appointment functionality", () => {
+    test.beforeEach("Login with valid creds", async ({ page }) => {
+    // 1. Lounch URL;
+    await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await expect(page).toHaveTitle("CURA Healthcare Service");
+    await expect(page.locator(".text-vertical-center h1")).toHaveText("CURA Healthcare Service",);
+
+    // 2. Click on the Make Appointment
+    await page.getByRole("link", { name: "Make Appointment" }).click();
+    await expect(page.getByText("Please login to make")).toBeVisible();
+
+    // 3.Successful Login
+    await page.getByLabel("Username").fill("John Doe");
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    // 4. Assert if the appointment page is loaded
+    await expect(page.locator("h2")).toContainText("Make Appointment");
+    });
+
+    test("Should make an appointment with non-default values", async ({ page }) => {
+        // Dropdown
+        await page.getByLabel("Facility").selectOption("Hongkong CURA Healthcare Center");
+
+        
+        // Checkbox
+        await page.getByText ("Apply for hospital readmission").click();
+        // Radio button
+        await page.getByText ("Medicaid").click();
+        // Date input box
+        await page.getByRole("textbox", { name: "Visit Date (Required)" }).click();
+        await page.getByRole("textbox", { name: "Visit Date (Required)" }).fill("05/10/2027");
+        await page.getByRole("textbox", { name: "Visit Date (Required)" }).press("Enter");
+
+        // Multi-line comments input box
+        await page. getByRole ("textbox", { name: "Comment" }). click();
+        await page.getByRole("textbox", { name: "Comment" }). fill("This is a multi-line comments\ncaptured today");
+        
+        // Button
+        await page. getByRole ("button", {name: "Book Appointment" }). click();
+        // Assertion
+        await expect (page. locator("h2")). toContainText ("Appointment Confirmation"); await expect (page.getByRole("Link", { name: "Go to Homepage" })). toBeVisible()
+        
+    });
+
+    // Nore tests 
+});
+
