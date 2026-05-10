d0085c4c42cdb36b51ea6f54b21c0efa6787d617
schirila13 <chirilasorin13@gmail.com>
2026-05-10 13:07:34 +0300

    add new inventory test file

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -1,14 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("should demo locators", async ({ page }) => {
-//
-
-
-
-await page.goto("https://katalon-demo-cura.herokuapp.com/");
-
-await page.getByRole("link", { name: "Make Appointment" }).click();
-await expect(page.getByText("Please login to make")).toBeVisible();
+test("should demo config file", async ({ page }) => {
 
 
 })
